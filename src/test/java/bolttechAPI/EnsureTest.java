c2870a0ce7195e3c6382e5ea69c144344905dd95
lucasvasquezdeoliveira <lucasvasquezdeoliveira@gmail.com>
2022-09-03 21:40:45 -0300

    teste 1

---
+++ src/test/java/bolttechAPI/EnsureTest.java
@@ -0,0 +1,19 @@
+package bolttechAPI;
+
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class EnsureTest {
+    @Test
+    public void testDadoumadministradorQaundocadastroumclienteEntaoobtenhoostatus201 () {
+
+        //Configurar caminho comum de acesso a API
+
+        //Login Api Rest com Admin
+
+        //Cadastar um cliente
+
+    }
+}
